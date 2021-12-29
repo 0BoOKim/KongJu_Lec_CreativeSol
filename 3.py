73cbb31b5d444ecdb726eac4e6d6e3ecf4c39e9e
0BoOKim <0bookim@gmail.com>
2021-12-29 22:37:08 +0900

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,7 +1,7 @@
 # 함수 명이 recursv( ) 일 때, 종료 조건이 없는 재귀함수 구조를 갖도록 코드를 작성하시오.
 
-def recursive_function():
+def recursv():
     
-    recursive_function()
+    recursv()
 
-recursive_function()
+recursv()
